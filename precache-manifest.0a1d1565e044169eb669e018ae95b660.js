f1ce5a830ea2b67abdf2be922314359f3d97d050
marcelo <marcelo170690@gmail.com>
2020-07-08 16:54:14 -0400

    deploy test 17

---
+++ precache-manifest.0a1d1565e044169eb669e018ae95b660.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01770421f0e9684e7800bf19232efe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5123fba6696c907fc4d3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e61609c4047be43bd1ab",
+    "url": "/static/css/main.adb13f8b.chunk.css"
+  },
+  {
+    "revision": "5123fba6696c907fc4d3",
+    "url": "/static/js/2.ddbd8250.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.ddbd8250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61609c4047be43bd1ab",
+    "url": "/static/js/main.a9d7d2e2.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "be8c0043a16fa58c23aabe72f37202e2",
+    "url": "/static/media/marceloNormal.be8c0043.png"
+  }
+]);
